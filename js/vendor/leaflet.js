b53314642576a9c41791f77d2fcbddb24a1a051d
Magdalena Dwulit <magda@Mareks-MBP.fios-router.home>
2019-07-24 08:17:52 -0400

    WIP: Update http to https

--- js/vendor/leaflet.js
+++ js/vendor/leaflet.js
@@ -1,5 +1,5 @@
 /*
- Leaflet, a JavaScript library for mobile-friendly interactive maps. http://leafletjs.com
+ Leaflet, a JavaScript library for mobile-friendly interactive maps. https://leafletjs.com
  (c) 2010-2013, Vladimir Agafonkin
  (c) 2010-2011, CloudMade
 */
